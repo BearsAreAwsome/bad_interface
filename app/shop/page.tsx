6a44a7fa7e42242daa5e73808d07ced4e396a9e0
Brooke Duvall <bmduvall10@gmail.com>
2024-11-20 18:39:34 -0500

    fix

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,12 +1,10 @@
 'use client'
-import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
 import Shop from "../shop";
 
 
 
 export default function Page() {
-  const router = useRouter()
   return (
     <div className="h-screen bg-cover bg-center" style={{backgroundImage: "url('images/earthboundBackground.gif')"}}>
       <Shop></Shop>
